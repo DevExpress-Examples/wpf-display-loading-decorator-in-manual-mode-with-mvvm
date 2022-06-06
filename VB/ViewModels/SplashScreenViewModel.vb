d9268c19092fac19f895442f00b50c07207c97c9
DevExpressExampleBot <fakeEmail>
2022-06-06 11:28:32 +0400

    Source auto update [skip ci]

--- VB/ViewModels/SplashScreenViewModel.vb
+++ VB/ViewModels/SplashScreenViewModel.vb
@@ -1,10 +1,5 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-
 Namespace DXSample.ViewModels
+
     Public Class SplashScreenViewModel
         Inherits MyViewModelBase
 
